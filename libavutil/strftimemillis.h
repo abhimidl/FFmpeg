e3afcfab3e6483c2048b31c09b57ffdd822aa0e4
abhimidl <abhishek@gomidl.com>
2023-11-01 00:49:13 +0530

    Changes

--- libavutil/strftimemillis.h
+++ libavutil/strftimemillis.h
@@ -1,11 +1,11 @@
+#ifndef AVUTIL_STRFTIMEMILLIS_H
+#define AVUTIL_STRFTIMEMILLIS_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
 #include <math.h>
-
-#ifndef AVUTIL_STRFTIMEMILLIS_H
-#define AVUTIL_STRFTIMEMILLIS_H
 
 size_t strftime_millis(char *ptr, size_t maxsize, const char *format, const struct timeval *tv);
 #endif
